10cb8a0daf5d7d655366ab944ea456328cbd5970
Sihle-Mazibuko <sihlemazii@gmail.com>
2024-07-06 23:43:29 +0200

    Test

---
+++ src/Components/About/AboutInfo.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const AboutInfo = () => {
+  return (
+    <div className="about-info grid ">
+      <div className="a-skills-data">
+        <div className="a-skills-title">
+          <h3 className="a-skills-name">Development</h3>
+          <span className="a-skills-number ">6%</span>
+        </div>
+
+        <div className="a-skills-bar">
+          <span className="a-skills-percentage dev"></span>
+        </div>
+      </div>
+
+      <div className="a-skills-data">
+        <div className="a-skills-title">
+          <h3 className="a-skills-name">UI/UX</h3>
+          <span className="a-skills-number  ">85%</span>
+        </div>
+
+        <div className="a-skills-bar">
+          <span className="a-skills-percentage ui"></span>
+        </div>
+      </div>
+
+      <div className="a-skills-data">
+        <div className="a-skills-title">
+          <h3 className="a-skills-name">Design</h3>
+          <span className="a-skills-number">80%</span>
+        </div>
+
+        <div className="a-skills-bar">
+          <span className="a-skills-percentage des"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutInfo;
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,7 +2,61 @@ import React from "react";
 import "./contact.css";
 
 const Contact = () => {
-  return <section className="section" id="contact"></section>;
+  return (
+    <section className="contact section" id="contact">
+      <h2 className="section-title">Contact</h2>
+      <span className="section-subtitle">Get in touch with me</span>
+
+      <div className="contact-contain contain grid">
+        <div className="contact-content">
+          <h3 className="contact-title">Talk to me</h3>
+
+          <div className="contact-info">
+            {/* email */}
+            <div className="contact-card">
+              <i className="bx uil- contact-card-icon"></i>
+
+              <h3 className="contact-card-title">Email</h3>
+              <span className="contact-card-data">sihlemazii@gmail.com</span>
+
+              <a href="mailto:sihlemazii@gmail.com" className="contact-button">
+                Write to me
+                <i className="bx uil-arrow-right contact-button-icon"></i>
+              </a>
+            </div>
+            {/* Whatsapp */}
+            <div className="contact-card">
+              <i className="bx uil- contact-card-icon"></i>
+
+              <h3 className="contact-card-title">Whatsapp</h3>
+              <span className="contact-card-data">(+27)-83-989-0907</span>
+
+              <a href="" className="contact-button">
+                Write to me
+                <i className="bx uil-arrow-right contact-button-icon"></i>
+              </a>
+            </div>
+            {/* instagram */}
+            <div className="contact-card">
+              <i className="bx uil- contact-card-icon"></i>
+
+              <h3 className="contact-card-title">Instagram</h3>
+              <span className="contact-card-data">sihle.jpeg</span>
+
+              <a href="" className="contact-button">
+                Write to me
+                <i className="bx uil-arrow-right contact-button-icon"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="contact-content">
+          <h3 className="contact-title">Write me your project</h3>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
