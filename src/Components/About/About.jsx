4a081f93025884d4037e6c2e3306857d99dbf7f9
sihle.js <sihlemazii@gmail.com>
2025-02-19 16:27:01 +0200

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -14,9 +14,10 @@ const About = () => {
         <div className="about-data">
           <AboutInfo />
           <p className="about-description">
-            I am a frontend and backend developer based in Johannesburg, South
-            Africa. I create functional websites, web applications, games and
-            useful software applications that leave clients satisfied.
+            I am a frontend developer based in Johannesburg, South Africa. I
+            create functional websites, web applications, games and useful
+            software applications while adhering to UI/UX standards to ensure
+            the best products.
           </p>
 
           <a download="" href={CV} className="button button-flex">
